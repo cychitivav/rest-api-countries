66bbe2f8a18ebaff0d3cee0b48fb1a52cb59005c
Cristian Chitiva <cychitivav@unal.edu.co>
2023-11-17 22:36:24 -0500

    Add Nunito font

--- rest-countries-api-with-color-theme-switcher/src/App.js
+++ rest-countries-api-with-color-theme-switcher/src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Detail from "./pages/Detail";
+import "./App.css";
+
 function App() {
-	return <div></div>;
+	return (
+		<>
+			<Header />
+			<Search />
+		</>
+	);
 }
 
 export default App;
