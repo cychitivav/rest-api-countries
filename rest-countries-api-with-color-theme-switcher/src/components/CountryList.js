669fe7b2d2ba7a4ab55e427b19430a75db5cd007
Cristian Chitiva <cychitivav@unal.edu.co>
2023-11-18 20:23:14 -0500

    Finish Country list components with filter

---
+++ rest-countries-api-with-color-theme-switcher/src/components/CountryList.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+
+function CountryList(props) {
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const response = await fetch("https://restcountries.com/v3.1/all");
+				const data = await response.json();
+				setCountries(data);
+			} catch (error) {
+				console.error("Error al obtener los datos:", error);
+			}
+		};
+
+		getData();
+	});
+
+	return (
+		<ul>
+			{countries.map((country) => {
+				if (
+					(props?.region !== "" ||
+						country?.region?.toLowerCase() === props?.region?.toLowerCase()) &&
+					country?.name?.common
+						?.toLowerCase()
+						.includes(props?.search?.toLowerCase())
+				) {
+					return (
+						<li key={country.cca3}>
+							<img src={country.flags.svg} alt={country.name.common} />
+							<h2>{country.name.common}</h2>
+							<p>
+								<strong>Population:</strong> {country.population}
+							</p>
+							<p>
+								<strong>Region:</strong> {country.region}
+							</p>
+							<p>
+								<strong>Capital:</strong> {country.capital}
+							</p>
+						</li>
+					);
+				} else {
+					return null;
+				}
+			})}
+		</ul>
+	);
+}
+
+export { CountryList };
