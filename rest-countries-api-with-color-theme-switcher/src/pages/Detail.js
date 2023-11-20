26ace9b66ace14523326710c8b0371ed8d6a4477
Cristian Chitiva <cychitivav@unal.edu.co>
2023-11-20 08:22:08 -0500

    Add styling and functionality to Detail page

--- rest-countries-api-with-color-theme-switcher/src/pages/Detail.js
+++ rest-countries-api-with-color-theme-switcher/src/pages/Detail.js
@@ -1,50 +1,72 @@
 import React from "react";
 import { Header } from "../components/Header";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Detail({ countries }) {
 	const { code } = useParams();
 
 	const country = countries.find((country) => country.cca3 === code);
+
 	return (
 		<>
 			<Header />
 			<nav>
-				<button onClick={() => (window.location.href = "/")}>
-					<span>←</span> Back
-				</button>
+				<Link to="/">
+					<button>
+						<span>←</span> Back{" "}
+					</button>
+				</Link>
 			</nav>
 
 			<main>
-				<img src={country.flags?.svg} alt={`${country.name?.common} flag`} />
-				<div>
-					<h2>{country.name?.common}</h2>
-					<div>
-						<p>
-							<strong>Native Name: </strong>
-							{country.name?.nativeName?.common} <br />
-							<strong>Population: </strong>{" "}
-							{country.population.toLocaleString()} <br />
-							<strong>Region: </strong> {country.region} <br />
-							<strong>Sub Region: </strong> {country.subregion} <br />
-							<strong>Capital: </strong> {country.capital}
-						</p>
-						<p>
-							<strong>Top Level Domain: </strong>
-							{country.tld.join(", ")} <br />
-							<strong>Currencies: </strong>
-							{Object.keys(country.currencies).join(", ")} <br />
-							<strong>Languages: </strong>
-							{Object.values(country.languages).join(", ")}
-						</p>
-					</div>
-					<p>
-						<strong>Border Countries: </strong>
-						{country.borders.map((border) => (
-							<span>{border} </span>
-						))}
-					</p>
-				</div>
+				{country ? (
+					<>
+						<img
+							src={country?.flags?.svg}
+							alt={`${country?.name?.common} flag`}
+						/>
+						<div>
+							<h2>{country?.name?.common}</h2>
+							<div>
+								<p>
+									<strong>Native Name: </strong>
+									{Object.values(country?.name?.nativeName)
+										.map((nativeName) => nativeName?.common)
+										.join(", ")}{" "}
+									<br />
+									<strong>Population: </strong>{" "}
+									{country?.population.toLocaleString()} <br />
+									<strong>Region: </strong> {country?.region} <br />
+									<strong>Sub Region: </strong> {country?.subregion} <br />
+									<strong>Capital: </strong> {country?.capital}
+								</p>
+								<p>
+									<strong>Top Level Domain: </strong>
+									{country?.tld.join(", ")} <br />
+									<strong>Currencies: </strong>
+									{Object.keys(country?.currencies).join(", ")} <br />
+									<strong>Languages: </strong>
+									{Object.values(country?.languages).join(", ")}
+								</p>
+							</div>
+							<p>
+								<strong>Border Countries: </strong>
+
+								{country?.borders.map((border) => (
+									<span>
+										{
+											countries.find((country) => country.cca3 === border).name
+												.common
+										}
+									</span>
+								))}
+							</p>
+						</div>
+					</>
+				) : (
+					<></>
+				)}
 			</main>
 		</>
 	);
